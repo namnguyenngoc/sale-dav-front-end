843cc6615ca3c60c7c7e404a902e96c37a69cd87
itnguyennamphoto <itnguyennamphoto@gmail.com>
2021-08-13 08:10:04 +0700

    change api url

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const API_URL = 'http://103.69.193.126:8789/'; //  platform server
 
 // Only developing application connect to local ip ,another applications connect to developement server ip.
 const config = {
-  API_FAMILY: `${API_URL}:80`,
+  API_FAMILY: `${API_URL}:8789`,
 //  API_FAMILY: `${API_URL}:8789`,
   dateTimeFormat: 'YYYYMMDD', // Format date time constant for condition search
   tooltipWidth: 300,
