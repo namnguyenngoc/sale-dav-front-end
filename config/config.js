fd0a0fa9ce4f8b505602cd81ecc39c7143a3d74e
namnguyenngoc <itnguyennam@gmail.com>
2021-08-17 11:19:06 +0700

    change to vps

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-const API_URL = 'http://anvatchibeo.ddns.net'; //  platform server
+const API_URL = 'http://103.69.193.126'; //  platform server
 // const API_URL = 'http://127.0.0.1' //  platform server
 
 // Only developing application connect to local ip ,another applications connect to developement server ip.
 const config = {
-  API_FAMILY: `${API_URL}:80/api`,
-//  API_FAMILY: `${API_URL}:8789`,
+  // API_FAMILY: `${API_URL}:80/api`,
+ API_FAMILY: `${API_URL}:8789`,
   dateTimeFormat: 'YYYYMMDD', // Format date time constant for condition search
   tooltipWidth: 300,
 }
